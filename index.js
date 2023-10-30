3f456632232b7c900cd939db68acd385dece1414
Leandro <leandrolopez131982@gmail.com>
2023-10-30 13:05:25 -0300

    el mejor de tres

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function eleccion(jugada) {
     let resultado = "";
     if (jugada == 1) {
         resultado = "piedra 🪨"
-    } else if(jugador == 2){
+    } else if(jugada == 2){
         resultado = "papel 📄"
-    } else if (jugador ==3){
+    } else if (jugada ==3){
         resultado = "tijera ✂️"
     } else{
        resultado = "MAL ELEJIDO"
@@ -19,7 +19,13 @@ return resultado
 let jugador = 0;
 let min = 1;
 let max = 3;
-let pc = aleatorio(1,3)
+let pc = 0;
+let triunfos = 0;
+let perdidas = 0;
+
+while(triunfos < 3 && perdidas < 3) {
+
+pc = aleatorio(1,3)
 jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
 
 alert("PC elige: " + eleccion(pc));
@@ -29,10 +35,17 @@ if (pc == jugador){
     alert("EMPATE")
 } else if(jugador == 1 && pc == 3){
     alert("GANASTE")
+    triunfos = triunfos + 1
 }else if(jugador == 2 && pc == 1){
     alert("GANASTE")
+    triunfos = triunfos + 1
 }else if(jugador == 3 && pc == 2){
         alert("GANASTE")
+    triunfos = triunfos + 1
 } else {
     alert("PERDISTE!!")
+    perdidas = perdidas + 1
 }
+}
+
+alert("ganaste " + triunfos + " veces, perdiste " + perdidas + " veces");
