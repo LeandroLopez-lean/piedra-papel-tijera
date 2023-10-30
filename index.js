6226209606365eda2a1113b085db18c511a38a2c
Leandro <leandrolopez131982@gmail.com>
2023-10-30 11:52:38 -0300

    if y else if

--- index.js
+++ index.js
@@ -4,11 +4,33 @@ jugador = prompt("Elige: 1 para piedra, 2 para papel, 3 para tijera");
 /* alert("Elejiste " + jugador); */
 
 if (jugador == 1) {
-    alert("Elejiste piedra")
+    alert("Elejiste 🪨")
 } else if(jugador == 2){
-    alert("Elejiste Papel")
+    alert("Elejiste 📄")
 } else if (jugador ==3){
-    alert("Elejiste Tijera")
+    alert("Elejiste ✂️")
 } else{
     alert("Elejiste PERDER!!!")
+}
+
+if (pc == 1) {
+    alert("PC Eleje 🪨")
+} else if(pc == 2){
+    alert("PC Eleje 📄")
+} else if (pc ==3){
+    alert("PC Eleje ✂️")
+} else{
+    alert("Elejiste PERDER!!!")
+}
+
+if (pc == jugador){
+    alert("EMPATE")
+} else if(jugador == 1 && pc == 3){
+    alert("GANASTE")
+}else if(jugador == 2 && pc == 1){
+    alert("GANASTE")
+}else if(jugador == 3 && pc == 2){
+        alert("GANASTE")
+} else {
+    alert("PERDISTE!!")
 }
